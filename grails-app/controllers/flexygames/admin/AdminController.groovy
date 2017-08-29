e3e5b0053f1bf59e0b2512af48585f503d507bc8
Vincent Frison <vincent.frison@gmail.com>
2017-08-30 00:49:23 +0200

    Session group duplication (for the next year) on the admin page.

--- grails-app/controllers/flexygames/admin/AdminController.groovy
+++ grails-app/controllers/flexygames/admin/AdminController.groovy
@@ -1,8 +1,11 @@
 package flexygames.admin
 
+import flexygames.SessionGroup
 import org.apache.shiro.SecurityUtils
 
-import flexygames.User;
+import flexygames.User
+
+import java.util.regex.Pattern;
 
 class AdminController {
 
@@ -24,5 +27,55 @@ class AdminController {
 		def t = '%' + params.playerToken + '%'
 		def users = User.findAllByUsernameIlikeOrFirstNameIlikeOrLastNameIlikeOrEmailIlike(t, t, t, t)
 		render (view: "index", model: [users: users, message: "" + users.size() + " user(s) has been found."])
+	}
+
+	def duplicateSessionGroup = {
+		def group = SessionGroup.get(params.sessionGroupId)
+		if (!group) {
+			flash.error = "Unable to find session group with id=" + params.sessionGroupId
+			return redirect(controller: 'admin', action: 'index')
+		}
+		def newName = duplicateNameToNextYear(group.name)
+		if (!newName) {
+			flash.error = "Unable to increment the years token in the name"
+			return redirect(controller: 'admin', action: 'index')
+		}
+		if (SessionGroup.findByName(newName)) {
+			flash.error = "A session group already exists for the next year !"
+			return redirect(controller: 'admin', action: 'index')
+		}
+		def newGroup = new SessionGroup(name: newName, competition: group.competition, visible: group.visible,
+				defaultType: group.defaultType, defaultPlayground: group.defaultPlayground, defaultDuration: group.defaultDuration,
+				defaultMinPlayerNbr: group.defaultMinPlayerNbr, defaultMaxPlayerNbr: group.defaultMaxPlayerNbr, defaultPreferredPlayerNbr: group.defaultPreferredPlayerNbr,
+				defaultLockingTime: group.defaultLockingTime, defaultDayOfWeek: group.defaultDayOfWeek,
+				ballsTaskNeeded: group.ballsTaskNeeded, jerseyTaskNeeded: group.jerseyTaskNeeded)
+		if (!newGroup.save()) {
+			flash.error = "Unable to save duplicated session group: " + newGroup.errors
+		} else {
+			flash.message = "Session group <b>" + newGroup.name + "</b> has been saved !"
+		}
+		return redirect(controller: 'admin', action: 'index')
+	}
+
+	private String duplicateNameToNextYear(String name) {
+		def result = null
+		def SIMPLE_YEAR_TOKEN = '[0-9]{4}'
+		def simpleYearMatcher = Pattern.compile(SIMPLE_YEAR_TOKEN).matcher(name)
+		def DOUBLE_YEAR_TOKEN = '[0-9]{4}/[0-9]{4}'
+		def doubleYearMatcher = Pattern.compile(DOUBLE_YEAR_TOKEN).matcher(name)
+		if (doubleYearMatcher.find()) {
+			int index = doubleYearMatcher.start()
+			String yearz = name.substring(index, index + 9)
+			String[] years = yearz.split('/')
+			int firstYear = Integer.parseInt(years[0]) + 1
+			int secondYear = Integer.parseInt(years[1]) + 1
+			String newYears = "" + firstYear + "/" + secondYear
+			result = name.replaceAll(DOUBLE_YEAR_TOKEN, newYears)
+		} else if (simpleYearMatcher.find()) {
+			int index = simpleYearMatcher.start()
+			int year = Integer.parseInt(name.substring(index, index + 4)) + 1
+			result = name.replaceAll(SIMPLE_YEAR_TOKEN, "" + year)
+		}
+		return result
 	}
 }
--- grails-app/views/admin/index.gsp
+++ grails-app/views/admin/index.gsp
@@ -8,14 +8,14 @@
 		<g:message code="admin.title" default="Administration" />
 	</h1>
 	<br />
-	<g:if test="${message}">
+	<g:if test="${flash.message}">
 		<div class="message">
-			${message}
+			${flash.message}
 		</div>
 	</g:if>
-	<g:if test="${error}">
+	<g:if test="${flash.error}">
 		<div class="errors">
-			${error}
+			${flash.error}
 		</div>
 	</g:if>
 	<g:message code="admin.refreshAllPlayersCounters" />
@@ -31,17 +31,23 @@
 	</g:form>
 	<br />
 	<table>
-	<g:each in="${users}" var="u">
-		<tr>
-			<td>${u.username}</td>
-			<td><g:link controller="player" action="show" id="${u.id}">View</g:link></td>
-			<td><g:link controller="user" action="edit" id="${u.id}">Edit</g:link></td>
-			<td>${u.firstName}</td>
-			<td>${u.lastName}</td>
-			<td><a href="mailto:${u.email}">${u.email}</a></td>
-		</tr>
-	</g:each>
+		<g:each in="${users}" var="u">
+			<tr>
+				<td>${u.username}</td>
+				<td><g:link controller="player" action="show" id="${u.id}">View</g:link></td>
+				<td><g:link controller="user" action="edit" id="${u.id}">Edit</g:link></td>
+				<td>${u.firstName}</td>
+				<td>${u.lastName}</td>
+				<td><a href="mailto:${u.email}">${u.email}</a></td>
+			</tr>
+		</g:each>
 	</table>
+	<br />
+	<g:form action="duplicateSessionGroup">
+		<g:message code="admin.duplicateSessionGroup" />
+		<g:select name="sessionGroupId" from="${flexygames.SessionGroup.list()}" optionKey="id" required="" />
+		<input type="submit" value="Duplicate" class="button" />
+	</g:form>
 	<br />
 	<br />
 	<br />
